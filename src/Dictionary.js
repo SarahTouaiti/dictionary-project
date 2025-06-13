3088ad8facb522b6d8921995127118c71d7a8088
SarahTouaiti <touaiti.sara@gmail.com>
2025-06-13 17:12:14 +0200

    React Dictionary Project / API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  function handleResponse(response) {
+    console.log(response.data);
+  }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} !`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
   return (
     <div className="dictionary">
