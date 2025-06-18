179f0a04985f21826ac02d5d0391578034d04b13
SarahTouaiti <touaiti.sara@gmail.com>
2025-06-18 12:53:11 +0200

    React Dictionary Project / Synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,18 +1,29 @@
 import React from "react";
+import Example from "./Example";
+import Synonyms from "./Synonyms";
+import "./Meaning.css";
+
 export default function Meaning(props) {
   return (
     <div className="meaning">
       <h4>{props.meaning.partOfSpeech}</h4>
-      {props.meaning.definitions.map(function (definition, index) {
-        return (
-          <div key={index}>
-            <p>
-              {definition.definition}
-              <em>{definition.example}</em>
-            </p>
-          </div>
-        );
-      })}
+      <ul>
+        {props.meaning.definitions.map(function (definition, index) {
+          return (
+            <li>
+              <div key={index}>
+                <p>
+                  {definition.definition}
+                  <br />
+                  <Example example={definition.example} />
+                  <br />
+                  <Synonyms synonyms={definition.synonyms} />
+                </p>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
